be8ab8c1f51d2f935474a622085243298d48c494
Dzeshiko <tetiana.ananieva@gmail.com>
2021-12-07 14:41:38 +0200

    New files

---
+++ block.cpp
@@ -0,0 +1,16 @@
+#include "pxt.h"
+#include <cstdint>
+#include <math.h>
+
+using namespace pxt;
+
+namespace spaceBlock {
+   /**
+    * textCpp
+    */
+    //% blockId=un_id block ="name %name"
+    //% color="#AA278D"
+    float fooCpp(int16_t name) {
+        return name / 1023.0;
+    }
+}  
---
+++ shims.d.ts
@@ -0,0 +1,12 @@
+// Auto-generated. Do not edit.
+declare namespace spaceBlock {
+
+    /**
+     * textCpp
+     */
+    //% blockId=un_id block ="name %name"
+    //% color="#AA278D" shim=spaceBlock::fooCpp
+    function fooCpp(name: int16): number;
+}
+
+// Auto-generated. Do not edit. Really.
